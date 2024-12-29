12aa01b87b95567287dd49d1f5f17e97838d1615
aswanthpvtk <paswanthvtk@gmail.com>
2024-12-29 08:00:13 +0530

    2nd

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
         </select>
     </div>
 
-    <!-- Updated grid to display 4 cards in a row with hover effect -->
     <div id="product-container" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-5">
        
     </div>
